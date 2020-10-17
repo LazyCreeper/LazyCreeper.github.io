13aa19523a8127197d1cd4fbe3cfb8756f8428b4
LazyCreeper <40060779+LazyCreeper@users.noreply.github.com>
2020-10-17 09:38:38 +0800

    兼容MDUI v1.0.0

--- js/docs.js
+++ js/docs.js
@@ -1,33 +1,39 @@
-// 代码高亮
-hljs.initHighlightingOnLoad();
+$(function() {
+  var $document = $(document);
 
-// 页面滚动
-smoothScroll.init({
-  speed: 300
-});
+  // 代码高亮
+  hljs.initHighlightingOnLoad();
 
-// 图片占位符
-Holder.addTheme("gray", {
-  bg: "#BCBEC0",
-  fg: "rgba(255, 255, 255, 1)",
-  size: 12,
-  fontweight: "normal"
-});
+  // 页面滚动
+  var scroll = new SmoothScroll('[data-scroll]', {
+    speed: 300,
+    speedAsDuration: true
+  });
 
-// 示例处理
-$$('.viewsource').on('click', function () {
-  var $this = $$(this);
-  $this.parents('.doc-example').eq(0).toggleClass('doc-example-showcode');
-});
+  $('.mdui-toolbar-spacer').on('click', function() {
+    scroll.animateScroll(0);
+  });
 
+  // 图片占位符
+  Holder.addTheme("gray", {
+    bg: "#BCBEC0",
+    fg: "rgba(255, 255, 255, 1)",
+    size: 12,
+    fontweight: "normal"
+  });
 
-/**
- * 设置文档主题
- */
-(function () {
-  var DEFAULT_PRIMARY = 'red';
-  var DEFAULT_ACCENT = 'indigo';
-  var DEFAULT_LAYOUT = '';
+  // 示例处理
+  $('.viewsource').on('click', function () {
+    var $this = $(this);
+    $this.parents('.doc-example').eq(0).toggleClass('doc-example-showcode');
+  });
+
+  /**
+   * 设置文档主题
+   */
+  var DEFAULT_PRIMARY = 'light-blue';
+  var DEFAULT_ACCENT = 'pink';
+  var DEFAULT_LAYOUT = 'auto';
 
   // 设置 cookie
   var setCookie = function (key, value) {
@@ -49,7 +55,7 @@ $$('.viewsource').on('click', function () {
     }
 
     var i, len;
-    var $body = $$('body');
+    var $body = $('body');
 
     var classStr = $body.attr('class');
     var classs = classStr.split(' ');
@@ -63,7 +69,7 @@ $$('.viewsource').on('click', function () {
       }
       $body.addClass('mdui-theme-primary-' + theme.primary);
       setCookie('docs-theme-primary', theme.primary);
-      $$('input[name="doc-theme-primary"][value="' + theme.primary + '"]').prop('checked', true);
+      $('input[name="doc-theme-primary"][value="' + theme.primary + '"]').prop('checked', true);
     }
 
     // 设置强调色
@@ -75,7 +81,7 @@ $$('.viewsource').on('click', function () {
       }
       $body.addClass('mdui-theme-accent-' + theme.accent);
       setCookie('docs-theme-accent', theme.accent);
-      $$('input[name="doc-theme-accent"][value="' + theme.accent + '"]').prop('checked', true);
+      $('input[name="doc-theme-accent"][value="' + theme.accent + '"]').prop('checked', true);
     }
 
     // 设置主题色
@@ -85,42 +91,51 @@ $$('.viewsource').on('click', function () {
           $body.removeClass(classs[i]);
         }
       }
-      if (theme.layout !== '') {
-        $body.addClass('mdui-theme-layout-' + theme.layout);
-      }
+      $body.addClass('mdui-theme-layout-' + theme.layout);
       setCookie('docs-theme-layout', theme.layout);
-      $$('input[name="doc-theme-layout"][value="' + theme.layout + '"]').prop('checked', true);
+      $('input[name="doc-theme-layout"][value="' + theme.layout + '"]').prop('checked', true);
     }
   };
 
   // 切换主色
-  $$(document).on('change', 'input[name="doc-theme-primary"]', function () {
+  $document.on('change', 'input[name="doc-theme-primary"]', function () {
     setDocsTheme({
-      primary: $$(this).val()
+      primary: $(this).val()
     });
   });
 
   // 切换强调色
-  $$(document).on('change', 'input[name="doc-theme-accent"]', function () {
+  $document.on('change', 'input[name="doc-theme-accent"]', function () {
     setDocsTheme({
-      accent: $$(this).val()
+      accent: $(this).val()
     });
   });
 
   // 切换主题色
-  $$(document).on('change', 'input[name="doc-theme-layout"]', function () {
+  $document.on('change', 'input[name="doc-theme-layout"]', function () {
     setDocsTheme({
-      layout: $$(this).val()
+      layout: $(this).val()
     });
   });
 
   // 恢复默认主题
-  $$(document).on('cancel.mdui.dialog', '#dialog-docs-theme', function () {
+  $document.on('cancel.mdui.dialog', '#dialog-docs-theme', function () {
     setDocsTheme({
       primary: DEFAULT_PRIMARY,
       accent: DEFAULT_ACCENT,
       layout: DEFAULT_LAYOUT
     });
   });
-})();
 
+  // 如果抽屉栏当前激活项不在视野中，则滚动抽屉栏，使激活项位于垂直居中
+  (function () {
+    var $drawer = $('#main-drawer');
+    var $activeItem = $drawer.find('.mdui-list-item-active');
+    var activeItemOffsetTop = $activeItem.offset().top;
+    var drawerHeight = $drawer.innerHeight();
+
+    if (activeItemOffsetTop - 64 < 0 || activeItemOffsetTop - 64 + 238 > drawerHeight) {
+      $drawer[0].scrollTop = activeItemOffsetTop + $drawer[0].scrollTop - drawerHeight / 2;
+    }
+  })();
+});
